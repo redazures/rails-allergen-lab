5bc39752fe119bad1a416a578dfd6ef7cccaabe5
redazures <jwu_nyc@yahoo.com>
2020-08-10 14:15:10 -0400

    done

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,19 +1,19 @@
 class RecipesController < ApplicationController
-    before_action :find_employee, only:[:show,:edit,:update,:destroy]
+    before_action :find_recipe, only:[:show,:edit,:update,:destroy]
 
     def index
         @recipes=Recipe.all
     end
-
+    def show;end
     def new
         @recipe=Recipe.new
     end
 
     def create
-        @recipe=Recipe.create(params[:recipe_params])
-        byebug
+        @recipe=Recipe.create(recipe_params)
+        # byebug
         if @recipe.valid?
-            redirect_to recipe_path(@recipe)
+            redirect_to recipes_path
         else
             flash[:my_errors]=@recipe.errors.full_messages
             redirect_to new_recipe_path
@@ -25,5 +25,7 @@ class RecipesController < ApplicationController
         params.require(:recipe).permit(:name, :content, :user_id)
     end
 
-    def 
+    def find_recipe
+        @recipe=Recipe.find(params[:id])
+    end
 end
